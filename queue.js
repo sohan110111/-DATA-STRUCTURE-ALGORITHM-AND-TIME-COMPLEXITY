d355af1926bbf80df64ea4e8d197ec0fdcb2217c
sohan110111 <sohan110111@gmail.com>
2021-02-23 12:25:52 +0600

    Queue Data Structure using some realistic examples

---
+++ queue.js
@@ -0,0 +1,27 @@
+// FIFO = First In First Out
+// example = webpage scroll
+
+class Queue {
+    constructor() {
+        this.queue = [];
+    }
+    enqueue(item) {
+        this.queue.push(item);
+    }
+    dequeue() {
+        if(this.queue.length) {
+            return this.queue.shift();
+        }
+    }
+}
+
+const premiks = new Queue();
+premiks.enqueue("Ashique");
+premiks.enqueue("Dewan");
+premiks.enqueue("Prem");
+premiks.enqueue("Priya");
+
+console.log(premiks.queue);
+const winner = premiks.dequeue();
+console.log(winner);
+console.log(premiks.queue);
